239fb0d6b089a3ebbb33e7e0445271105cfef601
andrueastman <andrueastman@gmail.com>
2016-06-23 08:32:04 +0300

    Can find contours of images now. Need to filter out the smaller contours and then apply image estimation mathematical formulae :)

---
+++ potato_size.py
@@ -0,0 +1,47 @@
+# Example USAGE
+# python potato_size.py -i potato2small.jpg
+
+# import the necessary packages
+from scipy.spatial import distance as dist
+from imutils import perspective
+from imutils import contours
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+def midpoint(ptA, ptB):
+	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to the input image")
+#ap.add_argument("-w", "--width", type=float, required=True, help="width of the left-most object in the image (in inches)")
+args = vars(ap.parse_args())
+
+# load the image, and resize it to 640 by 480 px
+image = cv2.imread(args["image"])
+newimage = cv2.resize(image,(640,480))
+cv2.imshow("Image", newimage)
+cv2.waitKey(0)
+
+# convert image to grayscale and slightly blur 
+gray = cv2.cvtColor(newimage, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (7, 7), 0)
+cv2.imshow("Image", gray)
+cv2.waitKey(0)
+
+# perform edge detection, then perform a dilation + erosion to
+# close gaps in between object edges
+edged = cv2.Canny(gray, 50, 100)
+edged = cv2.dilate(edged, None, iterations=1)
+edged = cv2.erode(edged, None, iterations=1)
+cv2.imshow("Image", edged)
+cv2.waitKey(0)
+
+# find contours in the edge map
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+cv2.drawContours(image, cnts, -1, (0,255,0), 3)
+cv2.imshow("Image", image)
+cv2.waitKey(0)
